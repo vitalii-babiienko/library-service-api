f0d54f72b900b80c4b9a88ed97dc776963d77eec
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-10-26 16:10:12 +0300

    The use of the necessary book serializer depends on the action

--- book/views.py
+++ book/views.py
@@ -2,10 +2,23 @@ from rest_framework import viewsets
 
 from book.models import Book
 from book.permissions import IsAdminOrReadOnly
-from book.serializers import BookSerializer
+from book.serializers import (
+    BookSerializer,
+    BookListSerializer,
+    BookDetailSerializer,
+)
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = (IsAdminOrReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+
+        if self.action == "retrieve":
+            return BookDetailSerializer
+
+        return BookSerializer
