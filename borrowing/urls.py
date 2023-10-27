1fff26d694cba410c0d85af067ad50bc847bfb85
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-10-27 16:26:44 +0300

    Register routes for the borrowing app

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,5 +1,10 @@
-urlpatterns = [
+from rest_framework import routers
 
-]
+from borrowing.views import BorrowingViewSet
+
+router = routers.DefaultRouter()
+router.register("", BorrowingViewSet)
+
+urlpatterns = router.urls
 
 app_name = "borrowing"
