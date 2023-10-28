90209c982316e9bd1ede754d0f69608b1d801ebb
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-10-28 14:39:49 +0300

    Implement filtering for the Borrowing List endpoint

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,4 +1,6 @@
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from borrowing.models import Borrowing
 from borrowing.serializers import (
@@ -11,15 +13,29 @@ from borrowing.serializers import (
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         queryset = self.queryset
+
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
 
         if self.action in ("list", "retrieve"):
             queryset = queryset.select_related(
                 "book",
                 "user",
             )
+
+        if self.action == "list":
+            user_id = self.request.query_params.get("user_id")
+            is_active = self.request.query_params.get("is_active")
+
+            if user_id:
+                queryset = queryset.filter(user_id=user_id)
+
+            if is_active:
+                queryset = queryset.filter(is_active=is_active.capitalize())
 
         return queryset
 
@@ -31,3 +47,22 @@ class BorrowingViewSet(viewsets.ModelViewSet):
             return BorrowingDetailSerializer
 
         return BorrowingSerializer
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="user_id",
+                type=int,
+                description="Filter by user id (ex. ?user_id=1)",
+                required=False,
+            ),
+            OpenApiParameter(
+                name="is_active",
+                type=bool,
+                description="Filter by borrowing status (ex. ?is_active=true)",
+                required=False,
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
